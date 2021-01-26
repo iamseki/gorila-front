a36c206b18454fa659ebdfbc541b073465e21f3a
iamseki <chrisyuri_19@hotmail.com>
2021-01-26 17:31:58 -0300

    retrieving api url accordlying to node_env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL ?? 'http://localhost:8080',
+  baseURL: process.env.NODE_ENV == "development" ? 'http://localhost:8080' : "https://gorila-challenge-api.herokuapp.com",
 });
 
 export default api;
