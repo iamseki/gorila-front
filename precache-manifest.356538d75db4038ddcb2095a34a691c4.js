0eaf2958c9633602caec62854157d638df37da49
iamseki <chrisyuri_19@hotmail.com>
2021-01-26 16:55:13 -0300

    Updates

---
+++ precache-manifest.356538d75db4038ddcb2095a34a691c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db605adc5a667916909236dfcec785c",
+    "url": "/gorila-front/index.html"
+  },
+  {
+    "revision": "3e49521bbd622be71b7e",
+    "url": "/gorila-front/static/css/2.f5f68e44.chunk.css"
+  },
+  {
+    "revision": "3e49521bbd622be71b7e",
+    "url": "/gorila-front/static/js/2.9ecbe093.chunk.js"
+  },
+  {
+    "revision": "56d3562459fcea4721e44fb33b19d1e2",
+    "url": "/gorila-front/static/js/2.9ecbe093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b38c067f41db61de11",
+    "url": "/gorila-front/static/js/main.28dff21d.chunk.js"
+  },
+  {
+    "revision": "ea04219a85809bb8c79f",
+    "url": "/gorila-front/static/js/runtime-main.369a7000.js"
+  },
+  {
+    "revision": "12a42cb5151c6fe8dbdd966820ee47ff",
+    "url": "/gorila-front/static/media/logo.12a42cb5.svg"
+  }
+]);
